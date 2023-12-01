06b12612cf42e503d1ad613ee44724617a2541dc
chiohaus <rocioitzel.g@gmail.com>
2023-12-01 10:45:39 -0600

    valida

--- Controlador/validarMenu.php
+++ Controlador/validarMenu.php
@@ -1,6 +1,6 @@
 
 <?php
-/*
+
 //COMPRUEBA QUE EL USUARIO ESTA AUTENTIFICADO
 if ($_SESSION['valido'] != '1') {
     // Llamada al menú de navegación
@@ -13,22 +13,8 @@ if ($_SESSION['valido'] != '1') {
     $menu = new menuLogin();
     $menu->barraMenuLogin();
 }
-*/
+
 ?>
 
-
-<?php
-if (isset($_SESSION['valido']) && $_SESSION['valido'] === '1') {
-    // Llamada al menú de navegación para usuarios autenticados
-    include 'menulogin.php';
-    $menu = new menuLogin();
-    $menu->barraMenuLogin();
-} else {
-    // Llamada al menú de navegación para usuarios no autenticados
-    include 'menu.php';
-    $menu = new menu();
-    $menu->barraMenu();
-}
-?>
 
 
